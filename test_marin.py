a5032969196309ac6d0b541854fcca08caed7b52
Marin <marinemma.teller@opilane.tammegymnaasium.ee>
2025-09-04 09:38:13 +0300

    Add files via upload

---
+++ test_marin.py
@@ -0,0 +1,11 @@
+from tkinter import *
+from tkinter import ttk
+
+raam =Tk()
+raam.title("Nimi")
+raam.geometry("300x160")
+
+tekstisilt = ttk.Label(raam, text="Marin")
+tekstisilt.place(x=130, y=60)
+
+raam.mainloop()
